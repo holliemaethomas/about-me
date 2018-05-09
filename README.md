102265c04bc4d6b3851948f1b6f55f3d516c3caa
holliemaethomas <holliemaethomas85@gmail.com>
2018-05-08 18:40:50 -0700

    working javascript

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 _This is my page about me!_
 
-#Purpose
+# Purpose
 The purpose of this page is to inform the viewer about the author of this page and to demonstrate current abilities with HTML, Javascript, and CSS. 
 
-###Software used/required
+### Software used/required
 This page was written in Visual Studio Code (Version 1.19.1) and can be viewed from any browser. 
 
 
-####Author
+#### Author
 The author of this page is Hollie M. Thomas, an introductory student at Code Fellows Coding Academy, Seattle Washington. 
 
 ###### helpful links
--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-alert('working?');
+var greeting = alert('Hello there! my name is Hollie');
+var extraPoints = alert('but there are some who call me Tim!')
+var userName = prompt('What is your name?');
+console.log ('the user entered ' + userName);
+var warn = prompt('ok ' + userName + ' we are going to play a little guessing game about me');
 
-//get user info
+var questOne = prompt('True or false, I grew up in Washington? Please answer T or F').toUpperCase();
+console.log('the user entered ' + questOne);
+if (questOne === 'T'){ alert('nope! I grew up in Cool, CA. it is a real town. google it!')
+} else (questOne === 'F'){ alert('correct! I grew up in Cool, CA. it is a real town. google it! ')} 
 
 //guessing game questions 
+//questions I really like movies, can you guess my favorite movie? I will give you a hint: 
