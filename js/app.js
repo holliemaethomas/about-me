8bbc9a5074229f4d228db5c9a306c15c945d3048
holliemaethomas <holliemaethomas85@gmail.com>
2018-05-08 20:45:06 -0700

    javascript working and complete

--- js/app.js
+++ js/app.js
@@ -21,11 +21,11 @@ if (questTwo === 'Y') {
   alert('Yup! I am the mom at the metal show. Good job those points are adding up!')
   userPoints += 1
 } else {
-  alert('Metal really is my favorite, especially Death Core and Black');
+  alert('Metal really is my favorite, especially Death Core and Black')
 }
 console.log('the user entered ' + questTwo + ' user has ' + userPoints);
 
-var questThree = prompt('Was I ever in the military?').toUpperCase();
+var questThree = prompt('Was I ever in the military?').toUpperCase()
 if (questThree === 'Y') {
   alert('I served six years in the army')
   userPoints += 1
@@ -34,13 +34,22 @@ if (questThree === 'Y') {
 }
 console.log('the user entered ' + questThree + ' user has ' + userPoints);
 
-var questFour = prompt('Are chocolate chip cookies my favorite?').toUpperCase();
+var questFour = prompt('Are chocolate chip cookies my favorite?').toUpperCase()
 if (questFour === 'Y') {
   alert('Tricked ya! i dont like cookies. at all. ever.');
-  userPoints += 1;
+  userPoints += 1
 } else {
   alert('Tricked ya! i dont like cookies. at all. ever.');
 }
 console.log('the user entered ' + questFour + ' user has ' + userPoints);
 
-var questFive = prompt 
+var questFive = prompt('Do I have animals?').toUpperCase()
+if (questFive === 'Y') {
+  alert('I have two dogs and a big stupid cat');
+  userPoints += 1
+} else {
+  alert('oh dear... I have three. Four if you count the toddler');
+}
+console.log('the user entered ' + questFive + ' user has ' + userPoints);
+
+alert('well ' + userName + ' you scored a total of ' + userPoints + + ' points ' + ' not bad!');
