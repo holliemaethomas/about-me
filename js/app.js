c42a4488050c16fc6c17ed7d5fdd7ef5c3b336c9
holliemaethomas <holliemaethomas85@gmail.com>
2018-05-09 19:24:02 -0700

    corrected some lintr issues

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 'use strict'
 // global
-var userPoints = 0
+var userPoints = 0;
 
-var greeting = alert('Hello there! my name is Hollie')
-var extraPoints = alert('but there are some who call me Tim!')
-var userName = prompt('What is your name?')
-console.log('the user entered ' + userName)
-var warn = prompt('ok ' + userName + ' we are going to play a little guessing game about me')
+var greeting = alert('Hello there! my name is Hollie');
+var extraPoints = alert('but there are some who call me Tim!');
+var userName = prompt('What is your name?');
+console.log('the user entered ' + userName);
+var warn = prompt('ok ' + userName + ' we are going to play a little guessing game about me');
 
-var questOne = prompt('Did I grow up in Washington? Please answer Y or N').toUpperCase()
+var questOne = prompt('Did I grow up in Washington? Please answer Y or N').toUpperCase();
 if (questOne === 'N') {
-  alert('You scored your first point! I grew up in Cool, California. it is a real town. google it!')
-  userPoints += 1
+  alert('You scored your first point! I grew up in Cool, California. it is a real town. google it!');
+  userPoints += 1;
 } else {
   alert('nope! I grew up in Cool, CA. It is a real town. google it! '); }
-console.log('the user entered ' + questOne + ' user has ' + userPoints)
+console.log('the user entered ' + questOne + ' user has ' + userPoints);
 
-var questTwo = prompt('Is my favorite music metal?').toUpperCase()
+var questTwo = prompt('Is my favorite music metal?').toUpperCase();
 if (questTwo === 'Y') {
-  alert('Yup! I am the mom at the metal show. Good job those points are adding up!')
-  userPoints += 1
+  alert('Yup! I am the mom at the metal show. Good job those points are adding up!');
+  userPoints += 1;
 } else {
-  alert('Metal really is my favorite, especially Death Core and Black')
+  alert('Metal really is my favorite, especially Death Core and Black');
 }
 console.log('the user entered ' + questTwo + ' user has ' + userPoints);
 
-var questThree = prompt('Was I ever in the military?').toUpperCase()
+var questThree = prompt('Was I ever in the military?').toUpperCase();
 if (questThree === 'Y') {
-  alert('I served six years in the army')
-  userPoints += 1
+  alert('I served six years in the army');
+  userPoints += 1;
 } else {
-  alert('oh, sorry but I did')
+  alert('oh, sorry but I did');
 }
 console.log('the user entered ' + questThree + ' user has ' + userPoints);
 
@@ -43,7 +43,7 @@ if (questFour === 'Y') {
 }
 console.log('the user entered ' + questFour + ' user has ' + userPoints);
 
-var questFive = prompt('Do I have animals?').toUpperCase()
+var questFive = prompt('Do I have animals?').toUpperCase();
 if (questFive === 'Y') {
   alert('I have two dogs and a big stupid cat');
   userPoints += 1
