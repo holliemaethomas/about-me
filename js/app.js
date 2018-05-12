4e8fcf4096d43cb628f843e1008c5ce24812bfca
holliemaethomas <holliemaethomas85@gmail.com>
2018-05-12 14:06:02 -0700

    working prblm seven

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 // global
-// var userPoints = 0
+var userPoints = 0
 
 // var greeting = alert('Hello there! my name is Hollie')
 // var extraPoints = alert('but there are some who call me Tim!')
@@ -52,35 +52,37 @@
 // }
 // console.log('the user entered ' + questFive + ' user has ' + userPoints)
 
-// var answer = 33;
-// var attempts = 4;
-// var questSix = parseInt(prompt('Lets change it up a bit! with all this awesome life experience how old do you think I am?'));
-// console.log('user entered ' + questSix);
+// var answer = 33
+// var attempts = 4
+// var questSix = parseInt(prompt('Lets change it up a bit! with all this awesome life experience how old do you think I am?'))
+// console.log('user entered ' + questSix)
 // while (attempts > 1 && questSix !== answer) {
-//   attempts--;
-//   questSix = parseInt(prompt('oh dear me, no. You have ' + attempts + ' tries left. Guess again.'));
-//   console.log(questSix + attempts);
+//   attempts--
+//   questSix = parseInt(prompt('oh dear me, no. You have ' + attempts + ' tries left. Guess again.'))
+//   console.log(questSix + attempts)
 // }
 // if (questSix == answer) {
-//   alert('correct I am ' + answer);
-//   userPoints += 1;
-// } else if (attempts == 1){
-//   alert('Well...you tried....I am 33');
+//   alert('correct I am ' + answer)
+//   userPoints += 1
+// } else if (attempts == 1) {
+//   alert('Well...you tried....I am 33')
 // }
+// console.log('the user entered ' + questSix + ' user has ' + userPoints)
 
-var favoriteMovies = ['fargo', 'war games', 'dark city', 'revenant', 'no country for old men', 'the goonies'];
+
+var favoriteMovies = [ 'FARGO ' , 'WAR GAMES ', ' DARK CITY', ' THE LAST UNICORN ', 'NO COUNTRY FOR OLD MEN ', ' THE GOONIES'];
 var movieAttempts = 6;
-var questSeven = prompt('OK, last question ' + 'userName' + '. Can you guess my favorite movie? I will give you a hint: The only winning move is not to play').toLocaleLowerCase;
-console.log('The user entered' + questSeven);
-while (movieAttempts > 1 && questSeven !== favoriteMovies) {
+var questSeven = prompt('OK, last question ' + 'userName' + '. Can you guess my favorite movie? I will give you a hint: The only winning move is not to play').toUpperCase();
+console.log('The user entered ' + questSeven);
+for (var i = 0; i < favoriteMovies.length; i++) {
+  while (movieAttempts > 1 && questSeven !== favoriteMovies.length);
   movieAttempts--;
-  questSeven = prompt('That is a good movie but not my favorite you have ' + movieAttempts + ' remaining');
+  questSeven = prompt('That is a good movie but not my favorite, you have ' + movieAttempts + ' remaining');
   console.log(questSeven + movieAttempts);
-  if (questSeven === [1]){
-    alert('Yessir! ' + [1] + ' is my favorite but, I would have also accepted ' + favoriteMovies);
-    // userPoints += 1; 
-  } else if (movieAttempts == 1) {
-    alert('and I thought we were friends, my favorite movie is War Games but I would have accepted' + favoriteMovies)
+  if (questSeven === favoriteMovies || favoriteMovies[1]) {
+    alert('Yessir! ' + favoriteMovies[1] + ' is my favorite but, I would have also accepted ' + favoriteMovies);
+    userPoints += 1;
+  } else if (movieAttempts === 1) {
+    alert('and I thought we were friends, my favorite movie is War Games but I would have accepted ' + favoriteMovies);
   }
 }
-
